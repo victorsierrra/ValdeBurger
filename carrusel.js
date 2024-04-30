0583cf5a33a5258bd70a5505b9cd965a8110dcfb
Victor <a27300@svalero.com>
2024-04-30 20:43:50 +0200

    Inicio de carrusel

---
+++ carrusel.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let Contador;
+    let slides = document.getElementsByClassName("slides")[0].getElementsByTagName("img");
+    for (Contador = 0; Contador < slides.length; Contador++) {
+        slides[Contador].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 7000); // Cambia imagen cada 2 segundos
+}
+
+document.querySelector(".prev").addEventListener("click", function(){
+    slideIndex -= 2;
+    showSlides();
+});
+
+document.querySelector(".next").addEventListener("click", function(){
+    showSlides();
+});
