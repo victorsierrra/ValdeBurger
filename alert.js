c0e7cd32a39120a3f6dd22ef159ede8eecae8765
Daniel <a27314@svalero.com>
2024-05-14 19:46:05 +0200

    zona privada creada

---
+++ alert.js
@@ -0,0 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const adminEmail = 'admin@example.com';
+    const adminPassword = 'admin123';
+    const employeeEmail = 'employee@example.com';
+    const employeePassword = 'employee123';
+
+    if (email === adminEmail && password === adminPassword) {
+        window.location.href = 'homeadmin.html';
+    } else if (email === employeeEmail && password === employeePassword) {
+        window.location.href = 'homeempleado.html';
+    } else {
+        Swal.fire("Access Denegade!");
+    }
+});
