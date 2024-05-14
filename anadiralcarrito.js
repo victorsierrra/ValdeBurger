839ab95229386a19167e9ce5d0cc231a7f57de7c
Daniel <a27314@svalero.com>
2024-05-14 21:50:57 +0200

    carrito hecho e implementado en carta

---
+++ anadiralcarrito.js
@@ -0,0 +1,24 @@
+
+    document.getElementById('viewCartButton').addEventListener('click', function() {
+        window.location.href = 'cart.html';
+    });
+
+    function addToCart(product, price) {
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+        let item = cart.find(item => item.product === product);
+
+        if (item) {
+            item.quantity++;
+        } else {
+            cart.push({ product, price, quantity: 1 });
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+        Swal.fire({
+            title: 'Producto añadido',
+            html: `<strong>${product}</strong> se ha añadido al carrito`,
+            icon: 'success',
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#e74c3c'
+        });
+    }
