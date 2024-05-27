5843c6f65d8ac09b7e71ca7fc584d542cc2ab871
Victor <a27300@svalero.com>
2024-05-27 13:37:52 +0200

    Delete y findAll de productos

--- pages/javascript/products.js
+++ pages/javascript/products.js
@@ -1,22 +1,22 @@
 const urlProducto = 'http://localhost:8080/ValdeBurgerBuck02/Controller?ACTION=PRODUCTO.FIND_ALL';
-
+let productosLoaded = false;
 const fetchProducto = async ()=> {
     try{
         const result = await fetch(urlProducto);
         const data = await result.json();
         console.log('Productos obtenidos de la API: ', data);
-        printEmployees(data);
+        printProducto(data);
     }catch(error){
         console.log('Error al obtener datos de la API: ', error);
     }
 }
 
-const printProducto = (producto) => {
+const printProducto = (productos) => {
     const table = document.getElementById('tabla-producto');
     const tbody = table.querySelector('tbody');
     table.style.display= 'table';
 
-    producto.forEach(productos => {
+    productos.forEach(producto => {
         const {
             _idProducto,
             _categoria,
@@ -28,12 +28,11 @@ const printProducto = (producto) => {
             _ruta_imagen_alergias1,
             _ruta_imagen_alergias2,
             _ruta_imagen_alergias3,
-        } = productos;
+        } = producto;
 
         const row = document.createElement('tr');
         row.innerHTML = `
         <td>${_idProducto}</td>
-        <td>${_categoria}</td>
         <td>${_nombre}</td>
         <td>${_descripcion}</td>
         <td>${_ingredientes}</td>
@@ -42,8 +41,53 @@ const printProducto = (producto) => {
         <td>${_ruta_imagen_alergias1}</td>
         <td>${_ruta_imagen_alergias2}</td>
         <td>${_ruta_imagen_alergias3}</td>
+        <td>${_categoria}</td>
         `;
         tbody.appendChild(row);
     })
 }
-//fetchProducto();
+const toggleProductos = () => {
+    const table = document.getElementById('tabla-producto');
+    if (table.style.display === 'none') {
+        if (!productosLoaded) {
+            fetchProducto();
+            productosLoaded = true;
+        } else {
+            table.style.display = 'table';
+        }
+    } else {
+        table.style.display = 'none';
+    }
+}
+
+const deleteProduct = async (idProducto) => {
+    try {
+        const urlDeleteProducto = `http://localhost:8080/ValdeBurgerBuck02/Controller?ACTION=PRODUCTO.DELETE&ID_PRODUCTO=${idProducto}`;
+        await fetch(urlDeleteProducto, {
+            method: 'DELETE'
+        })
+            alert('Producto eliminado.');
+    } catch (error) {
+        console.error('Error al eliminar al producto:', error);
+    }
+};
+const toggleProductDelete = () => {
+    const formulario = document.getElementById('formulario-delete');
+    if(formulario.style.display === 'none'){
+        formulario.style.display = 'block'
+    }
+    else{
+        formulario.style.display = 'none'
+    }
+}
+
+
+document.getElementById('formulario-delete').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const idProducto = document.getElementById('casilla-id').value;
+    if (idProducto) {
+        deleteProduct(idProducto);
+    } else {
+        alert('Debe ingresar un ID válido.');
+    }
+});
